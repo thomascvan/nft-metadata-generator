11acc0e173711a951109fff327c94e7f5b5e7f12
Thomas Van <thomascvan11@gmail.com>
2023-08-09 18:29:03 -0700

    generated metadata

--- metadataGenerator.js
+++ metadataGenerator.js
@@ -7,7 +7,7 @@ for (let tokenId = 1; tokenId <= 40; tokenId++) {
   const metadata = {
     name: `Crypto Noire #${tokenId}`,
     description: `NFT Collection for Crypto Noire's Inaugural Retreat of Black Crypto Leaders.`,
-    image: `ipfs://QmdbGsym85gUMXW3MtKhSCZKt1HyUkxpAypqehgh3RLYFD`,
+    image: `ipfs://QmSLh7tarnVUfyFusqFEt893pHYTp8kqsaNFSyStPuLDB7/crypto-noire.png`,
     attributes: [
       {
         display_type: "date",
