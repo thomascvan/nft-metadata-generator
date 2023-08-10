f12a22f79d584d83109f858bb341e5851362177a
Thomas Van <thomascvan11@gmail.com>
2023-08-09 17:18:54 -0700

    added metadata generation functionality

--- metadataGenerator.js
+++ metadataGenerator.js
@@ -1,17 +1,25 @@
-// Convert the date to a Unix timestamp
-const date = new Date("August 17, 2023");
-const unixTimestamp = Math.floor(date.getTime() / 1000);
+const fs = require('fs');
 
-// Use the unixTimestamp as the value in your metadata
-const metadata = {
-  name: `Crypto Noire #${tokenId}`,
-  description: `NFT Collection for Crypto Noire's inaugural retreat of Black crypto Leaders.`,
-  image: `ipfs://QmdbGsym85gUMXW3MtKhSCZKt1HyUkxpAypqehgh3RLYFD`,
-  attributes: [
-    {
-      display_type: "date",
-      trait_type: "Inaugural Retreat",
-      value: unixTimestamp,
-    },
-  ],
-};
+for (let tokenId = 1; tokenId <= 40; tokenId++) {
+  const date = new Date("August 17, 2023");
+  const unixTimestamp = Math.floor(date.getTime() / 1000);
+
+  const metadata = {
+    name: `Crypto Noire #${tokenId}`,
+    description: `NFT Collection for Crypto Noire's Inaugural Retreat of Black Crypto Leaders.`,
+    image: `ipfs://QmdbGsym85gUMXW3MtKhSCZKt1HyUkxpAypqehgh3RLYFD`,
+    attributes: [
+      {
+        display_type: "date",
+        trait_type: "Inaugural Retreat",
+        value: unixTimestamp,
+      },
+    ],
+  };
+
+  const fileName = `${tokenId}.json`;
+
+  fs.writeFileSync(fileName, JSON.stringify(metadata, null, 2));
+
+  console.log(`Created ${fileName}`);
+}
